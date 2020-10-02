6aca31ae3dd3a063d96a0edfae33df3b67287d47
Naeem Model <me@nmode.ca>
2020-10-02 17:47:25 -0400

    Create State interface and implementations

---
+++ src/main/java/ca/nmode/hopcroft/states/State.java
@@ -0,0 +1,8 @@
+package ca.nmode.hopcroft.states;
+
+/**
+ * An immutable condition or property of a finite-state machine at a given step during a simulation.
+ *
+ * @author Naeem Model
+ */
+public interface State {}
