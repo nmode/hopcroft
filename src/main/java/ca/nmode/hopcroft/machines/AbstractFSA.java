36e10c4b4314c263b263d3680c2e537189b0747e
Naeem Model <me@nmode.ca>
2020-10-14 23:46:32 -0400

    Create skeletal implementations for finite-state acceptors

---
+++ src/main/java/ca/nmode/hopcroft/machines/AbstractFSA.java
@@ -0,0 +1,26 @@
+package ca.nmode.hopcroft.machines;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import ca.nmode.hopcroft.states.State;
+
+/* A skeletal implementation of a finite-state acceptor. */
+abstract class AbstractFSA<S extends State, I, K, V> extends AbstractFSM<S, I, K, V> {
+    final Set<S> acceptStates;
+
+    AbstractFSA(Set<S> states, Set<I> inputElements, Map<K, V> transitions, S startState, Set<S> acceptStates) {
+        super(states, inputElements, transitions, startState);
+
+        // Ensure the set of accept states is not null and is a subset of the set of states.
+        if (acceptStates == null)
+            throw new NullPointerException(
+                    "Cannot construct a finite-state acceptor whose set of accept states is null.");
+        if (!this.states.containsAll(acceptStates))
+            throw new IllegalArgumentException("Cannot construct a finite-state acceptor whose set of accept states is "
+                    + "not a subset of its set of states.");
+        this.acceptStates = Collections.unmodifiableSet(new HashSet<>(acceptStates));
+    }
+}
---
+++ src/main/java/ca/nmode/hopcroft/machines/AbstractNFSA.java
@@ -0,0 +1,69 @@
+package ca.nmode.hopcroft.machines;
+
+import java.util.Map;
+import java.util.Set;
+
+import ca.nmode.hopcroft.states.State;
+
+/**
+ * A skeletal implementation of a {@link NondeterministicFSA nondeterministic finite-state acceptor}. Extending classes
+ * need not validate the set of {@link #states() states}, set of {@link #inputElements() input elements},
+ * {@link #startState() start state} and set of {@link #acceptStates() accept states}. It is ensured that the
+ * {@link #transitions() transition map} neither is nor contains keys or values that are {@code null}.
+ * 
+ * @param <S> the type of this nondeterministic finite-state acceptor's states
+ * @param <I> the type of this nondeterministic finite-state acceptor's input elements
+ * @param <K> the type of the keys of this nondeterministic finite-state acceptor's transition map
+ * @param <V> the type of the values of this nondeterministic finite-state acceptor's transition map
+ * @param <C> the type of this nondeterministic finite-state acceptor's computations
+ *
+ * @author Naeem Model
+ */
+public abstract class AbstractNFSA<S extends State, I, K, V, C> extends AbstractFSA<S, I, K, V>
+        implements NondeterministicFSA<S, I, K, V, C> {
+    /**
+     * Constructs a nondeterministic finite-state acceptor given a set of states, set of input elements, transition map,
+     * start state and set of accept states.
+     * 
+     * @param states        the set of states of the new nondeterministic finite-state acceptor
+     * @param inputElements the set of input elements of the new nondeterministic finite-state acceptor
+     * @param transitions   the transition map of the new nondeterministic finite-state acceptor
+     * @param startState    the start state of the new nondeterministic finite-state acceptor
+     * @param acceptStates  the set of accept states of the new nondeterministic finite-state acceptor
+     * 
+     * @throws NullPointerException     if {@code states}, {@code inputElements}, {@code transitions} or
+     *                                  {@code acceptStates} is {@code null}; {@code states} or {@code inputElements}
+     *                                  contains {@code null}; or {@code transitions} contains {@code null} keys or
+     *                                  values
+     * @throws IllegalArgumentException if {@code states} is empty, {@code startState} is not in {@code states}, or
+     *                                  {@code acceptStates} is not a subset of {@code states}
+     */
+    public AbstractNFSA(Set<S> states, Set<I> inputElements, Map<K, V> transitions, S startState, Set<S> acceptStates) {
+        super(states, inputElements, transitions, startState, acceptStates);
+    }
+
+    @Override
+    public final Set<S> states() {
+        return states;
+    }
+
+    @Override
+    public final Set<I> inputElements() {
+        return inputElements;
+    }
+
+    @Override
+    public final Map<K, V> transitions() {
+        return transitions;
+    }
+
+    @Override
+    public final S startState() {
+        return startState;
+    }
+
+    @Override
+    public final Set<S> acceptStates() {
+        return acceptStates;
+    }
+}
