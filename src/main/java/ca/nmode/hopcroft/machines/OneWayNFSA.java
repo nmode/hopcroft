4be44b1fd6b76a90902b47658f1205fa7e98fd76
Naeem Model <me@nmode.ca>
2020-12-14 21:05:30 -0500

    Define additional constructors for one-way NFSAs

--- src/main/java/ca/nmode/hopcroft/machines/OneWayNFSA.java
+++ src/main/java/ca/nmode/hopcroft/machines/OneWayNFSA.java
@@ -3,6 +3,7 @@ package ca.nmode.hopcroft.machines;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -61,6 +62,46 @@ public class OneWayNFSA<S, I> extends AbstractNFSA<S, I, Entry<S, I>, Set<S>, Li
      */
     public OneWayNFSA(Set<S> states, Set<I> inputElements, Map<Entry<S, I>, Set<S>> transitions, S startState) {
         this(states, inputElements, transitions, startState, Set.of());
+    }
+
+    /**
+     * Constructs a one-way nondeterministic finite-state acceptor with the specified nondeterministic finite-state
+     * machine's set of states, set of input elements, transition map and start state, as well as the specified set of
+     * accept states.
+     * 
+     * @param n            the nondeterministic finite-state machine whose set of states, set of input elements,
+     *                     transition map and start state is used to construct the new one-way nondeterministic
+     *                     finite-state acceptor
+     * @param acceptStates the set of accept states of the new one-way nondeterministic finite-state acceptor
+     * 
+     * @throws NullPointerException     if {@code n} or {@code acceptStates} is {@code null}
+     * @throws IllegalArgumentException if {@code acceptStates} is not a subset of {@code n}'s states; {@code n}'s
+     *                                  transition map contains keys whose state or element is not in its set of states
+     *                                  and set of input elements, respectively; or {@code n}'s transition map contains
+     *                                  values that are not subsets of its set of states
+     */
+    public OneWayNFSA(NFSM<S, I, Entry<S, I>, Set<S>, ?> n, Set<S> acceptStates) {
+        this(Objects.requireNonNull(n,
+                "Cannot construct a one-way nondeterministic finite-state acceptor from a null "
+                        + "nondeterministic finite-state machine.")
+                .states(), n.inputElements(), n.transitions(), n.startState(), acceptStates);
+    }
+
+    /**
+     * Constructs a one-way nondeterministic finite-state acceptor with the specified nondeterministic finite-state
+     * machine's set of states, set of input elements, transition map and start state, as well as an empty set of accept
+     * states.
+     * 
+     * @param n the nondeterministic finite-state machine whose set of states, set of input elements, transition map and
+     *          start state is used to construct the new one-way nondeterministic finite-state acceptor
+     * 
+     * @throws NullPointerException     if {@code n} is {@code null}
+     * @throws IllegalArgumentException if {{@code n}'s transition map contains keys whose state or element is not in
+     *                                  its set of states and set of input elements, respectively; or {@code n}'s
+     *                                  transition map contains values that are not subsets of its set of states
+     */
+    public OneWayNFSA(NFSM<S, I, Entry<S, I>, Set<S>, ?> n) {
+        this(n, Set.of());
     }
 
     @Override
